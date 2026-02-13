d77f3933a1ee24dc8e37fe92cf511b6b8afc7a82
Your Name <yourmail@example.com>
2026-02-13 12:42:12 +0530

    old backend files deleted

--- varun/repository/courserepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.varun.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.varun.model.coursemodel;
-
-public interface courserepository extends JpaRepository<coursemodel, Long> {
-	boolean existsByNameIgnoreCase(String name);
-}
--- varun/repository/grouprepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.varun.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.varun.model.groupmodel;
-
-public interface grouprepository extends JpaRepository<groupmodel, Long> {
-	groupmodel findByGroupName(String groupName);
-}
