7e9bef5a76bc94ca22a86e808933dea23d11e617
Your Name <yourmail@example.com>
2026-02-13 13:32:53 +0530

    just modified to cehck the clarity about the backend

--- varunjava/src/main/java/com/example/varun/dto/Sample.java
+++ varunjava/src/main/java/com/example/varun/dto/Sample.java
@@ -3,6 +3,6 @@ package com.example.varun.dto;
 public class Sample {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println(" hey man this is muthu");
 	}
 }
